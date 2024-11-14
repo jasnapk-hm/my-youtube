3bd24c5ac495213a6005096c658127a0788e1576
jasnapk-hm <hmjasnapk@gmail.com>
2024-11-14 11:45:33 +0530

    updated

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,3 @@
 
-// const GOOGLE_API_KEY='AIzaSyCOopXjWTiCaOFaLL46TwRUF2ZhaU4Phh'
 
-export const YOUTUBE_API ="https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=US&key="+GOOGLE_API_KEY;
+export const YOUTUBE_API ="https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=US&key=";
